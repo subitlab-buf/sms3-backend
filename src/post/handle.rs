8aa5b8d735857f37153f6378553edc5227d1362c
Yjn024 <61787533+JieningYu@users.noreply.github.com>
2023-05-21 21:40:52 +0800

    cargo update

--- src/post/handle.rs
+++ src/post/handle.rs
@@ -1,8 +1,7 @@
 use crate::account::Permission;
 use crate::post::cache::PostImageCache;
-use crate::post::Post;
 use crate::RequirePermissionContext;
-use std::todo;
+use chrono::NaiveDate;
 use tide::prelude::*;
 use tide::Request;
 
@@ -68,4 +67,74 @@ pub async fn cache_image(mut req: Request<()>) -> tide::Result {
             .into(),
         ),
     }
+}
+
+pub async fn post(mut req: Request<()>) -> tide::Result {
+    let cxt = match RequirePermissionContext::from_header(&req) {
+        Some(e) => e,
+        None => {
+            return Ok::<tide::Response, tide::Error>(
+                json!({
+                    "status": "error",
+                    "error": "Permission denied",
+                })
+                .into(),
+            )
+        }
+    };
+    match cxt.valid(vec![Permission::Post]).await {
+        Ok(able) => {
+            if able {
+                let id;
+                super::cache::INSTANCE
+                    .push(
+                        match PostImageCache::new(&req.body_bytes().await?, cxt.user_id) {
+                            Ok(e) => {
+                                id = e.1;
+                                e.0
+                            }
+                            Err(err) => {
+                                return Ok::<tide::Response, tide::Error>(
+                                    json!({
+                                        "status": "error",
+                                        "error": format!("Image cache create failed: {}", err),
+                                    })
+                                    .into(),
+                                )
+                            }
+                        },
+                    )
+                    .await;
+                Ok::<tide::Response, tide::Error>(
+                    json!({
+                        "status": "success",
+                        "cache_id": id,
+                    })
+                    .into(),
+                )
+            } else {
+                Ok::<tide::Response, tide::Error>(
+                    json!({
+                        "status": "error",
+                        "error": "Permission denied",
+                    })
+                    .into(),
+                )
+            }
+        }
+        Err(err) => Ok::<tide::Response, tide::Error>(
+            json!({
+                "status": "error",
+                "error": err.to_string(),
+            })
+            .into(),
+        ),
+    }
+}
+
+struct PostDescriber {
+    title: String,
+    description: String,
+    time_range: (NaiveDate, NaiveDate),
+    images: Vec<u64>,
 }
